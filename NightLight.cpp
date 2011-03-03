6af2f0ed579ac0023f2d2da8aa90d96760160404
Mark Fickett <mark.fickett@gmail.com>
2011-03-03 12:16:30 -0500

    Change NightLight to avoid flickering and switch gradually, and have only 'on' and 'off' levels, not a varied response. Adjust the threshold (come on only in darker situations).

--- NightLight.cpp
+++ NightLight.cpp
@@ -2,20 +2,54 @@
 
 #include "WProgram.h"
 
+#define SWITCH_DELAY	1000	// Wait SWITCH_DELAY ms before switching.
+#define SWITCH_TIME	2000	// Gradually change over SWITCH_TIME ms.
+#define VALUE_ON	255	// analog value for full-on
+#define VALUE_OFF	0	// analog value for full-off
+boolean isOn, shouldBeOn;
+unsigned long lastChangeMillis;
+int currentValue;
+
 void NightLight::setup()
 {
 	pinMode(PIN_LED_LIGHT, OUTPUT);
+	analogWrite(PIN_LED_LIGHT, VALUE_OFF);
+	isOn = shouldBeOn = false;
+	lastChangeMillis = millis();
+	currentValue = 0;
 }
 
 void NightLight::updateLight()
 {
 	int v = analogRead(PIN_SENSOR_PHOTO);
-	if (v <= PHOTO_SENSOR_MIN) {
-		digitalWrite(PIN_LED_LIGHT, LOW);
-	} else {
-		v = constrain(v, PHOTO_SENSOR_MIN, PHOTO_SENSOR_MAX);
-		analogWrite(PIN_LED_LIGHT,
-			map(v, PHOTO_SENSOR_MIN, PHOTO_SENSOR_MAX, 0, 1023));
+
+	unsigned long currentMillis = millis();
+	unsigned long elapsedMillis = currentMillis - lastChangeMillis;
+
+	if(v >= PHOTO_SENSOR_THRESHOLD != shouldBeOn) {
+		shouldBeOn = !shouldBeOn;
+		lastChangeMillis = currentMillis;
+	} else if (isOn != shouldBeOn && elapsedMillis >= SWITCH_DELAY) {
+		isOn = shouldBeOn;
+	} else if (currentValue != (isOn ? VALUE_ON : VALUE_OFF)) {
+		elapsedMillis = constrain(elapsedMillis, 0, SWITCH_TIME);
+		currentValue = map(elapsedMillis, SWITCH_DELAY, SWITCH_TIME,
+			isOn ? VALUE_OFF : VALUE_ON,
+			isOn ? VALUE_ON : VALUE_OFF);
+
+		//Serial.print("NightLight new value: ");
+		//Serial.println(currentValue);
+		switch(currentValue) {
+			case 0:
+				digitalWrite(PIN_LED_LIGHT, LOW);
+				break;
+			case 1023:
+				digitalWrite(PIN_LED_LIGHT, HIGH);
+				break;
+			default:
+				analogWrite(PIN_LED_LIGHT, currentValue);
+				break;
+		}
 	}
 }
 
--- NightLight.h
+++ NightLight.h
@@ -1,16 +1,22 @@
 #pragma once
 
 /**
- * Control an LED's brightness, making it inversely proportional
- * to sensed ambient light (with a full-off threshold).
+ * Switch an LED on or off (gradually and avoiding flickering)
+ *	based on sensed ambient light.
  */
 
 #define PIN_SENSOR_PHOTO	A0
 #define PIN_LED_LIGHT		9	// with PWM
-#define PHOTO_SENSOR_MIN	512
-#define PHOTO_SENSOR_MAX	920
+#define PHOTO_SENSOR_THRESHOLD	900
+// Sensor values:
+//	direct sunlight		12
+//	bright room, sunny day	156-123
+//	indoor lights		245
+//	dim room, sunny day	560	
+//	basement, sunny day	975-880
 
 namespace NightLight {
 	void setup();
 	void updateLight();
 };
+
